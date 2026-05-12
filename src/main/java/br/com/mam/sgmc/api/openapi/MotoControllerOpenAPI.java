bc58a50b29dd68f7b9153a3aa9163a3669ede68f
lorhansouza-wmtrading <lorhan.souza@wmtrading.com.br>
2026-05-12 13:45:21 -0300

    :books: docs: criar documentação dos endpoints relacionados a moto

--- src/main/java/br/com/mam/sgmc/api/openapi/MotoControllerOpenAPI.java
+++ src/main/java/br/com/mam/sgmc/api/openapi/MotoControllerOpenAPI.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.mam.sgmc.api.dto.request.MotoRequestDTO;
 import br.com.mam.sgmc.api.dto.response.MotoResponseDTO;
@@ -20,7 +21,8 @@ public interface MotoControllerOpenAPI {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Moto salva com sucesso"),
             @ApiResponse(responseCode = "400", description = "Requisição inválida"),
-            @ApiResponse(responseCode = "404", description = "Membro não encontrado")
+            @ApiResponse(responseCode = "404", description = "Membro não encontrado"),
+            @ApiResponse(responseCode = "409", description = "Placa já cadastrada")
     })
     public ResponseEntity<Void> criarMoto(@RequestBody MotoRequestDTO motoRequestDTO);
 
@@ -28,13 +30,21 @@ public interface MotoControllerOpenAPI {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Lista de motos"),
     })
-    public List<MotoResponseDTO> listarMotos();
+    public List<MotoResponseDTO> listarMotos(
+            @RequestParam(required = false) Long idMembro,
+            @RequestParam(required = false) String modelo,
+            @RequestParam(required = false) String marca,
+            @RequestParam(required = false) String seguro
+    );
 
     @Operation(summary = "Atualizar moto")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Moto atualizada com sucesso"),
             @ApiResponse(responseCode = "400", description = "Requisição inválida"),
-            @ApiResponse(responseCode = "404", description = "Moto não encontrada")
+            @ApiResponse(responseCode = "404", description = "Moto não encontrada (placa errada) ou membro não encontrado"),
+            @ApiResponse(responseCode = "409", description = "Moto não pode ser associada a um membro inativo")
     })
-    public ResponseEntity<MotoResponseDTO> atualizarMoto(@PathVariable String placa, @RequestBody MotoRequestDTO motoRequestDTO);
+    public ResponseEntity<MotoResponseDTO> atualizarMoto(
+            @PathVariable String placa,
+            @RequestBody MotoRequestDTO motoRequestDTO);
 }
