20948a9d3fe3a0ab371315769ddbadd0b671e132
lorhansouza-wmtrading <lorhan.souza@wmtrading.com.br>
2026-05-15 15:05:05 -0300

    :books: docs: criar documentação dos endpoints do controlador de localização

--- src/main/java/br/com/mam/sgmc/api/openapi/LocalizacaoControllerOpenAPI.java
+++ src/main/java/br/com/mam/sgmc/api/openapi/LocalizacaoControllerOpenAPI.java
@@ -18,30 +18,33 @@ public interface LocalizacaoControllerOpenAPI {
 
     @Operation(summary = "Listar Paises")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Paises listados com sucesso")
+            @ApiResponse(responseCode = "200", description = "Paises listados com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Paises nao encontrados")
     })
     public ResponseEntity<List<PaisResponseDTO>> listarPaisesComFiltros(
-        @RequestParam(required = false) String sigla,
-        @RequestParam(required = false) String nome,
-        @RequestParam(required = false) String continente);
+            @RequestParam(required = false) String sigla,
+            @RequestParam(required = false) String nome,
+            @RequestParam(required = false) String continente);
 
     @Operation(summary = "Listar UFs")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "UFs listadas com sucesso")
+            @ApiResponse(responseCode = "200", description = "UFs listadas com sucesso"),
+            @ApiResponse(responseCode = "404", description = "UFs nao encontradas")
     })
     public ResponseEntity<List<UfResponseDTO>> listarUfsComFiltros(
-        @RequestParam(required = false) String sigla,
-        @RequestParam(required = false) String nome,
-        @RequestParam(required = false) String regiao,
-        @RequestParam(required = false) String pais);
+            @RequestParam(required = false) String sigla,
+            @RequestParam(required = false) String nome,
+            @RequestParam(required = false) String regiao,
+            @RequestParam(required = false) String pais);
 
     @Operation(summary = "Listar Cidades")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Cidades listadas com sucesso")
+            @ApiResponse(responseCode = "200", description = "Cidades listadas com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Cidades nao encontradas")
     })
     public ResponseEntity<List<CidadeResponseDTO>> listarCidades(
-        @RequestParam(required = false) String ufSigla,
-        @RequestParam(required = false) String nome,
-        @RequestParam(required = false) String pais);
+            @RequestParam(required = false) String ufSigla,
+            @RequestParam(required = false) String nome,
+            @RequestParam(required = false) String pais);
 
 }
