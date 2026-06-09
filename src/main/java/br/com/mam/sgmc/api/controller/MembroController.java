a30908783be9a7daaff89e86cc9b44da7b48e96e
Lorhan Souza <lorhan.s160@gmail.com>
2026-06-09 07:03:52 -0300

    :hammer: refactor: alterar endpoint `DELETE` -> `PATCH`

--- src/main/java/br/com/mam/sgmc/api/controller/MembroController.java
+++ src/main/java/br/com/mam/sgmc/api/controller/MembroController.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -141,7 +141,7 @@ public class MembroController implements MembroControllerOpenAPI {
         return ResponseEntity.status(HttpStatus.OK).body(MembroResponseDTO.toResponseDTO(membro));
     }
 
-    @DeleteMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<Void> inativarMembro(@PathVariable Long id) {
         this.membroService.inativarMembro(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
