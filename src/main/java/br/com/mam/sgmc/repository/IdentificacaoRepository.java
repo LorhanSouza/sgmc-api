be93b60084f1924276f151f8863fc06906ce6e26
Lorhan Souza <lorhan.s160@gmail.com>
2026-06-17 00:36:54 -0300

    :wastebasket: remove: remover `@Repository` desnecessários

--- src/main/java/br/com/mam/sgmc/repository/IdentificacaoRepository.java
+++ src/main/java/br/com/mam/sgmc/repository/IdentificacaoRepository.java
@@ -1,10 +1,8 @@
 package br.com.mam.sgmc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import br.com.mam.sgmc.model.Identificacao;
 
-@Repository
 public interface IdentificacaoRepository extends JpaRepository<Identificacao, Long> {
 }
